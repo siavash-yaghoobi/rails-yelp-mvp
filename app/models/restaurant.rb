29503bf2262dd846d91406dcd1beca5823d99a47
Siavash Yaghoobi <siavashy13@gmail.com>
2020-02-19 12:53:23 +0100

    added review model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
 end
