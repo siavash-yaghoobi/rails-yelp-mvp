ee6192344e223622117358c95b836f8ef2617c68
Siavash Yaghoobi <siavashy13@gmail.com>
2020-02-19 16:21:01 +0100

    Created both controllers

--- Gemfile
+++ Gemfile
@@ -17,7 +17,8 @@ gem 'uglifier', '>= 1.3.0'
 gem 'webpacker'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'mini_racer', platforms: :ruby
-
+gem 'faker'
+# added the faker gem above check https://github.com/faker-ruby/faker for more info
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts "Creating restaurants"
+
+10.times do
+  type = %w(Chinese Italian Japanese French Belgian)
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.street_address,
+    category: type.sample
+    )
+end
+
+puts "Created 10 restaurants"
