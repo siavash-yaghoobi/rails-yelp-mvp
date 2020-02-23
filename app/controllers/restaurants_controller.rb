b557ec4a3a121efc5e4bb35ecabc59946085f631
Siavash Yaghoobi <siavashy13@gmail.com>
2020-02-23 21:54:01 +0100

    completed backend

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -4,13 +4,16 @@ class RestaurantsController < ApplicationController
   end
 
   def new
-    @restaurants = Restaurant.new
-    Restaurant.create(restaurant_params)
-    #redirect
+    @restaurant = Restaurant.new
   end
 
   def create
-    #??
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   def show
@@ -19,5 +22,6 @@ class RestaurantsController < ApplicationController
 
   private
   def restaurant_params
-    params.require(:restaurant).permit(:name,:address, :category, :phone_number)
+    params.require(:restaurant).permit(:name, :address, :category, :phone_number)
+  end
 end
